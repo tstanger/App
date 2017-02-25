159e6a108771f84e23bd198f249d8b131345d33e
Bryann07 <bhnguyen1000@gmail.com>
2017-02-24 22:38:09 -0600

    Add files via upload

---
+++ Excuses.java
@@ -0,0 +1,51 @@
+package GottaBlast;
+import java.util.Scanner;
+import java.util.Random;
+public class Excuses {
+
+
+public static void main(String[] args){
+	
+	boolean parents;
+	boolean siblings;
+	boolean pets;
+	boolean children;
+	
+	Scanner input = new Scanner(System.in);
+	Random num = new Random();
+	//System.out.println(siblings);
+	//System.out.println(parents);
+	
+	System.out.println("Do you have siblings? (True/False)");
+	siblings = input.nextBoolean();
+	input.nextLine();
+	System.out.println("Do you have atleast one parent? (True/False)");
+	parents = input.nextBoolean();
+	input.nextLine();
+	System.out.println("Do you have any pets? (True/False)");
+	pets = input.nextBoolean();
+	input.nextLine();
+	System.out.println("Do you have atleast one child? (True/False");
+	children = input.nextBoolean();
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
